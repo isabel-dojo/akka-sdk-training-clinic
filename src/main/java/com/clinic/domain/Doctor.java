d24d17e8ea5ff20e5d8b0675bf3d27d27e1db939
Lukasz Marchewka <lukasz.marchewka@gmail.com>
2025-10-17 12:26:06 +0200

    domain model for Doctor

---
+++ src/main/java/com/clinic/domain/Doctor.java
@@ -0,0 +1,15 @@
+package com.clinic.domain;
+
+import java.util.List;
+import java.util.Optional;
+
+public record Doctor(
+        String firstName,
+        String lastName,
+        List<String> specialities,
+        String description,
+        Optional<Contact> contact
+) {
+    public record Contact(Optional<String> phone, Optional<String> email) {
+    }
+}
